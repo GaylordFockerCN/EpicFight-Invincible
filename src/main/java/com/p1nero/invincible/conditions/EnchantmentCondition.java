bb8d8cf08c8351e28661e0ac4f1ef37e670f91b3
P1nero <1070402601@qq.com>
2025-02-17 10:58:06 +0800

    换supplier，改区间判断

--- src/main/java/com/p1nero/invincible/conditions/EnchantmentCondition.java
+++ src/main/java/com/p1nero/invincible/conditions/EnchantmentCondition.java
@@ -4,29 +4,44 @@ import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import yesman.epicfight.world.capabilities.entitypatch.player.ServerPlayerPatch;
 
+import java.util.function.Supplier;
+
 /**
  * 检测手上物品附魔的等级
  */
 public class EnchantmentCondition implements Condition<ServerPlayerPatch>{
     private final boolean isMainHand;
-    private final Enchantment enchantment;
-    private final int enchantmentLevel;
+    private final Supplier<Enchantment> enchantmentSupplier;
+    private final int min, max;
 
     /**
-     *
      * @param isMainHand 是否主手，false则检测副手
-     * @param enchantment 附魔种类
-     * @param enchantmentLevel 附魔等级
+     * @param enchantmentSupplier 附魔种类
+     * @param min 最小附魔等级
+     * @param max 最大附魔等级
      */
-    public EnchantmentCondition(boolean isMainHand, Enchantment enchantment, int enchantmentLevel) {
+    public EnchantmentCondition(boolean isMainHand, Supplier<Enchantment> enchantmentSupplier, int min, int max) {
         this.isMainHand = isMainHand;
-        this.enchantment = enchantment;
-        this.enchantmentLevel = enchantmentLevel;
+        this.enchantmentSupplier = enchantmentSupplier;
+        this.min = min;
+        this.max = max;
+    }
+    /**
+     * @param isMainHand 是否主手，false则检测副手
+     * @param enchantmentSupplier 附魔种类
+     * @param level 附魔等级
+     */
+    public EnchantmentCondition(boolean isMainHand, Supplier<Enchantment> enchantmentSupplier, int level) {
+        this.isMainHand = isMainHand;
+        this.enchantmentSupplier = enchantmentSupplier;
+        this.min = level;
+        this.max = level;
     }
 
     @Override
     public boolean predicate(ServerPlayerPatch serverPlayerPatch) {
-        return EnchantmentHelper.getItemEnchantmentLevel(enchantment, isMainHand ? serverPlayerPatch.getOriginal().getMainHandItem() : serverPlayerPatch.getOriginal().getOffhandItem()) == enchantmentLevel;
+        int level = EnchantmentHelper.getItemEnchantmentLevel(enchantmentSupplier.get(), isMainHand ? serverPlayerPatch.getOriginal().getMainHandItem() : serverPlayerPatch.getOriginal().getOffhandItem());
+        return  level >= min && level <= max;
     }
 
 }
