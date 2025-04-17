f6fb0bd395faf0880931622ef25cb41c8b4d0221
P1nero <1070402601@qq.com>
2025-04-17 22:59:12 +0800

    坚不可摧适配

--- src/main/java/com/p1nero/invincible/conditions/TargetBlockingCondition.java
+++ src/main/java/com/p1nero/invincible/conditions/TargetBlockingCondition.java
@@ -1,7 +1,6 @@
 package com.p1nero.invincible.conditions;
 
 import com.nameless.indestructible.world.capability.AdvancedCustomHumanoidMobPatch;
-import com.p1nero.invincible.mixin.AdvancedCustomHumanoidMobPatchAccessor;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.level.ServerPlayer;
@@ -11,16 +10,11 @@ import yesman.epicfight.data.conditions.Condition;
 import yesman.epicfight.skill.SkillContainer;
 import yesman.epicfight.skill.SkillSlots;
 import yesman.epicfight.world.capabilities.EpicFightCapabilities;
-import yesman.epicfight.world.capabilities.entitypatch.LivingEntityPatch;
-import yesman.epicfight.world.capabilities.entitypatch.player.PlayerPatch;
 import yesman.epicfight.world.capabilities.entitypatch.player.ServerPlayerPatch;
 import yesman.epicfight.world.capabilities.item.CapabilityItem;
 
 import java.util.List;
 
-/**
- * 仅在坚不可摧（indestructible）加载时可以使用
- */
 public class TargetBlockingCondition implements Condition<ServerPlayerPatch> {
 
     public TargetBlockingCondition() {
